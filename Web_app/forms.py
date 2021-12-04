3f8908cba34f445d984de656717df2cab1546a35
SCOTT <weiliangtuina@gmail.com>
2021-12-04 20:59:37 +0800

    style the contact form with form control.

--- Web_app/forms.py
+++ Web_app/forms.py
@@ -10,10 +10,19 @@ class ContactForm(forms.Form):
 
 class ContactModelForm(forms.ModelForm):
 	name = forms.CharField(label="Enter your name:", 
+		help_text='This is the help message, 100 characters max',
 		widget=forms.TextInput(attrs={
 			'class':'form-control'
-		}
-		))
+		}))
+	email = forms.EmailField(label="Enter your Email:",
+		widget=forms.EmailInput(attrs={
+			'class':'form-control'
+			}))
+	message = forms.CharField(
+		label="Leave us a message:",
+		widget=forms.Textarea(attrs={
+			'class':'form-control'
+			}))
 
 	class Meta:
 		model=Contact
